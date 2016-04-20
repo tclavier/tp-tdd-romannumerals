a881f4af727aa5d41be3a4e0ebf129fed7f43b22
Thomas Clavier <tom@tcweb.org>
2016-04-20 21:46:10 +0200

    template RomanNumerals

---
+++ src/main/java/tdd/RomanNumerals.java
@@ -0,0 +1,12 @@
+package tdd;
+
+public class RomanNumerals {
+	public String convertToRoman(String arabe) {
+		return "I";
+	}
+
+	public Object convertFromRoman(String roman) {
+		return "1";
+	}
+
+}
